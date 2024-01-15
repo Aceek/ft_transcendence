5fa1ef4005785fdab15a40f63af7c6a8b39890c0
rlouvrie <rlouvrie@student.42.fr>
2024-01-15 13:56:52 +0100

    adding 2fa and mail validation

--- srcs/backend/email_verification/urls.py
+++ srcs/backend/email_verification/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import TwoFactorVerifyView, VerifyEmailView
 
 urlpatterns = [
-
+    path("validate/", VerifyEmailView.as_view(), name="verify-email"),
+    path("2fa/", TwoFactorVerifyView.as_view(), name="2fa"),
 ]
--- srcs/backend/email_verification/utils.py
+++ srcs/backend/email_verification/utils.py
@@ -1,10 +1,10 @@
 
-import os
 from django.core.mail import send_mail
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-from django.urls import reverse
 from django.contrib.auth.tokens import default_token_generator
+from os import environ
+from .models import TwoFactorEmailModel
 
 
 def send_verification_email(user):
@@ -12,7 +12,7 @@ def send_verification_email(user):
         return
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
-    domain = os.environ.get("DOMAIN")
+    domain = environ.get("DOMAIN")
     # url_api = reverse("validate") # attendre l'implementation de la view
     url_api = "/api/users/validate/" # tmp solution
     verification_link = f"{domain}{url_api}?uid={uid}&token={token}/"
@@ -22,3 +22,8 @@ def send_verification_email(user):
     recipient_list = [user.email]
 
     send_mail(subject, message, None, recipient_list, fail_silently=False)
+
+def initiate_2fa(user):
+    instance_2fa = TwoFactorEmailModel.objects.create(user=user)
+    instance_2fa.send_two_factor_email()
+    return instance_2fa
