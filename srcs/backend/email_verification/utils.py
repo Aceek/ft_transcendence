679987ecb8d2d2d923a11b554a8387557bba87b0
rlouvrie <rlouvrie@student.42.fr>
2024-01-15 14:16:28 +0100

    updating func send_verification_email

--- srcs/backend/email_verification/utils.py
+++ srcs/backend/email_verification/utils.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from django.urls import reverse
 from django.contrib.auth.tokens import default_token_generator
 from os import environ
 from .models import TwoFactorEmailModel
@@ -12,8 +13,7 @@ def send_verification_email(user):
     uid = urlsafe_base64_encode(force_bytes(user.pk))
     token = default_token_generator.make_token(user)
     domain = environ.get("DOMAIN")
-    # url_api = reverse("validate") # attendre l'implementation de la view
-    url_api = "/api/mail/validate/"  # tmp solution
+    url_api = reverse("validate")
     verification_link = f"{domain}{url_api}?uid={uid}&token={token}/"
 
     subject = "Vérification d'e-mail"
