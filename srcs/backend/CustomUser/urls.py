97cdf9fffae0dbb85d15274fcd8bd2a2caead014
Aceek <linhardtilan@gmail.com>
2024-01-20 22:10:48 +0100

    rework on CustomUser api implement jwt to retrieve authenticate user

--- srcs/backend/CustomUser/urls.py
+++ srcs/backend/CustomUser/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CustomUserViewSet
+from .views import CustomUserListView, CustomUserDetailView, CustomUserFriendView
 
-router = default_router = routers.DefaultRouter()
-router.register(r"users", CustomUserViewSet, basename="users")
+# router = default_router = routers.DefaultRouter()
+# router.register(r"users", CustomUserListView, basename="users")
 
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("users", CustomUserListView.as_view()),
+    path("users/profile", CustomUserDetailView.as_view()),
+    path("users/remove_friends", CustomUserFriendView.as_view()),
+    # path("", include(router.urls)),
 ]
--- srcs/backend/CustomUser/views.py
+++ srcs/backend/CustomUser/views.py
@@ -7,31 +7,74 @@ from .serializers import CustomUserSerializer
 from .serializers import CustomUserSerializerFriend
 from rest_framework import exceptions
 from rest_framework import mixins
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 
-class CustomUserViewSet(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
+# class CustomUserListView(
+#     mixins.RetrieveModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.ListModelMixin,
+#     viewsets.GenericViewSet,
+# ):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = CustomUserSerializer
+
+#     def get_serializer_class(self):
+#         if self.action == "remove_friends" or self.action == "update_friends":
+#             return CustomUserSerializerFriend
+#         return super().get_serializer_class()
+
+
+class CustomUserListView(APIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get_serializer_class(self):
-        if self.action == "remove_friends" or self.action == "update_friends":
-            return CustomUserSerializerFriend
-        return super().get_serializer_class()
+    # GET /users/ retrieve all users
+    def get(self, request, format=None):
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data)
 
-    @action(detail=True, methods=["patch"])
-    def remove_friends(self, request, pk=None):
-        user = self.get_object()
-        serializer = self.get_serializer(data=request.data)
+    # PATCH /users/ update friends base on jwt token
+    def patch(self, request, format=None):
+        user = request.user
+        serializer = CustomUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class CustomUserDetailView(APIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    # GET /users/profile/ retrieve user profile base on jwt token
+    def get(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+
+
+class CustomUserFriendView(APIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializerFriend
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request):
+        user = request.user
+        serializer = CustomUserSerializerFriend(data=request.data)
+
         if serializer.is_valid():
             friends_to_remove = serializer.validated_data["friends"]
             for friend_id in friends_to_remove:
                 user.friends.remove(friend_id)
             user.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return_serializer = CustomUserSerializerFriend(user)
+            return Response(return_serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
