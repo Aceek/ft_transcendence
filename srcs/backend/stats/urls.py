eb94365d0504f6c0476ca3df31a228a132c28c38
Aceek <linhardtilan@gmail.com>
2024-01-21 23:25:41 +0100

    add historique retrieve method by id or by jwt

--- srcs/backend/stats/urls.py
+++ srcs/backend/stats/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
-from rest_framework import routers
-from .views import StatsView, MatchHistoryView
+from .views import StatsView, MatchHistoryView, MatchHistoryViewById
 
-router = routers.DefaultRouter()
-# router.register(r"stats", StatsView)
-router.register(r"match_history", MatchHistoryView, basename="MatchHistory")
 
 urlpatterns = [
+    # Stats view
     path("stats/me", StatsView.as_view(), name="stats"),
     path("stats/<uuid:user_id>", StatsView.as_view(), name="stats_by_user_id"),
-    path("", include(router.urls)),
+    # Match history view
+    path("history/me", MatchHistoryView.as_view(), name="history"),
+    path(
+        "history/<uuid:user_id>",
+        MatchHistoryViewById.as_view(),
+        name="history_by_user_id",
+    ),
 ]
--- srcs/backend/stats/views.py
+++ srcs/backend/stats/views.py
@@ -1,7 +1,5 @@
 from .models import Stats, MatchHistory
 from .serializers import StatsSerializer, MatchHistorySerializer
-from rest_framework import viewsets
-from rest_framework import mixins
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -10,13 +8,42 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 
-class MatchHistoryView(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+class MatchHistoryView(APIView):
     serializer_class = MatchHistorySerializer
-    queryset = MatchHistory.objects.none()
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        user = self.request.user
+    def get_match_history(self, user):
         return MatchHistory.objects.filter(Q(user1=user) | Q(user2=user))
+
+    def get(self, request):
+        """
+        Get user match history based on auth
+        """
+        match_history = self.get_match_history(request.user)
+
+        serializer = self.serializer_class(match_history, many=True)
+        return Response(serializer.data)
+
+    
+class MatchHistoryViewById(APIView):
+    serializer_class = MatchHistorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_user(self, user_id):
+        return get_object_or_404(CustomUser, id=user_id)
+
+    def get_match_history(self, user):
+        return MatchHistory.objects.filter(Q(user1=user) | Q(user2=user))
+
+    def get(self, request, user_id=None):
+        """
+        Get user match history based on auth or user_id
+        """
+        user = self.get_user(user_id)
+        match_history = self.get_match_history(user)
+
+        serializer = self.serializer_class(match_history, many=True)
+        return Response(serializer.data)
 
 
 class StatsView(APIView):
