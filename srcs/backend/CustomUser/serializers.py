78df16ddd2e33f3461c97076910ff1e03e959183
rlouvrie <rlouvrie@student.42.fr>
2024-02-29 16:47:02 +0100

    updating user serializer to avoid update when email already exist and when oauth user only try to change its email

--- srcs/backend/CustomUser/serializers.py
+++ srcs/backend/CustomUser/serializers.py
@@ -18,6 +18,41 @@ class CustomUserSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["id", "email"]
 
+    def validate_email_uniqueness(self, email):
+        """_summary_
+            verify if the email is not already used by another user
+
+        Args:
+            email (_type_): email to verify
+        """
+        try:
+            CustomUser.objects.get(email=email)
+            raise serializers.ValidationError("Email already exists")
+        except CustomUser.DoesNotExist:
+            return
+
+    def validate_oauth_only_email_change(self):
+        """_summary_
+            verify if the user is an oauth user only
+        """
+        if not self.instance.has_usable_password():
+            raise serializers.ValidationError("Oauth user can't change their email")
+
+    def validate_new_email(self, new_email):
+        """_summary_
+            validate the new_email field
+
+        Args:
+            new_email (_type_): new email to validate
+
+        Returns:
+            _type_: new_email if it's valid
+        """
+        self.validate_email_uniqueness(new_email)
+        self.validate_oauth_only_email_change()
+        return new_email
+        
+
     def update(self, instance, validated_data):
         """_summary_
             override the update method to handle the new_email and friends fields
@@ -30,7 +65,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
             _type_: call the super update method who will update the instance
         """
         new_email = validated_data.pop("new_email", None)
-        if new_email:
+        if new_email: 
             instance.new_email = new_email
             send_verification_email(instance, new_email)
         friends = validated_data.pop("friends", [])
