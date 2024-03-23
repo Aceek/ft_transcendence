49acfeb483f498cb563b033f3c931eb9728ec08f
pbeheyt <pierre.beheyt.bba@edhec.com>
2024-03-23 20:32:53 +0100

    load css before html for pong display

--- srcs/frontend/app/src/public/js/pong/displayPong.js
+++ srcs/frontend/app/src/public/js/pong/displayPong.js
@@ -5,9 +5,9 @@ import { setupGame } from "./main.js";
 export async function getPongGamePage() {
 
   try {
+    loadProfileCss("/public/css/pong.css");
     const pongHtml = await fetchTemplate("/public/html/pong.html");
     document.getElementById("main").innerHTML = pongHtml;
-    loadProfileCss("/public/css/pong.css");
   
     await loadScript("/public/js/pong/main.js").then(async() => {
         await setupGame();
